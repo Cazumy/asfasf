b3086162d925e30ccf59c97931ae3c28b49d6783
Cazumy <112489015+Cazumy@users.noreply.github.com>
2024-03-23 12:14:53 +0300

    Update Refrigeration_table.cpp

--- asfasf/Refrigeration_table.cpp
+++ asfasf/Refrigeration_table.cpp
@@ -1,31 +1,31 @@
 #pragma once
 #include "Refrigeration_table.h"
-	std::string Refrigeration_table::getClassName() const {
-		return "холодильный стол";
-	}
-	void Refrigeration_table::setSmh() {
-		int number;
-		std::cout << "Введите количество ящиков: ";
-		std::cin >> number; setNumber_of_box(number);
-		int temperature;
-		std::cout << "Введите минимально допустимую температуру: "; std::cin >> temperature;
-		setTemperature(temperature);
-		float power;
-		std::cout << "Укажите мощность модели: "; std::cin >> power;
-		setPower_of_table(power);
-	}
+std::string Refrigeration_table::getClassName() const {
+	return "С…РѕР»РѕРґРёР»СЊРЅС‹Р№ СЃС‚РѕР»";
+}
+void Refrigeration_table::setSmh() {
+	int number;
+	std::cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЏС‰РёРєРѕРІ: ";
+	std::cin >> number; setNumber_of_box(number);
+	int temperature;
+	std::cout << "Р’РІРµРґРёС‚Рµ РјРёРЅРёРјР°Р»СЊРЅРѕ РґРѕРїСѓСЃС‚РёРјСѓСЋ С‚РµРјРїРµСЂР°С‚СѓСЂСѓ: "; std::cin >> temperature;
+	setTemperature(temperature);
+	float power;
+	std::cout << "РЈРєР°Р¶РёС‚Рµ РјРѕС‰РЅРѕСЃС‚СЊ РјРѕРґРµР»Рё: "; std::cin >> power;
+	setPower_of_table(power);
+}
 
-	void Refrigeration_table::displaySmh()const  {
-		std::cout << "\n\tМинимальная температура: " << getTemperature();
-		std::cout << "\n\tКоличество ящиков: " << getNumber_of_box();
-		std::cout << "\n\tМощность модели: " << getPower_of_table();
-	}
+void Refrigeration_table::displaySmh()const  {
+	std::cout << "\n\tРњРёРЅРёРјР°Р»СЊРЅР°СЏ С‚РµРјРїРµСЂР°С‚СѓСЂР°: " << getTemperature();
+	std::cout << "\n\tРљРѕР»РёС‡РµСЃС‚РІРѕ СЏС‰РёРєРѕРІ: " << getNumber_of_box();
+	std::cout << "\n\tРњРѕС‰РЅРѕСЃС‚СЊ РјРѕРґРµР»Рё: " << getPower_of_table();
+}
 
-	void Refrigeration_table::setTemperature(int temp) { min_temperature_of_Table = temp; } // установка минимально температуры 
-	int Refrigeration_table::getTemperature() const { return min_temperature_of_Table; } // вывод минимально температуры 
+void Refrigeration_table::setTemperature(int temp) { min_temperature_of_Table = temp; } // СѓСЃС‚Р°РЅРѕРІРєР° РјРёРЅРёРјР°Р»СЊРЅРѕ С‚РµРјРїРµСЂР°С‚СѓСЂС‹ 
+int Refrigeration_table::getTemperature() const { return min_temperature_of_Table; } // РІС‹РІРѕРґ РјРёРЅРёРјР°Р»СЊРЅРѕ С‚РµРјРїРµСЂР°С‚СѓСЂС‹ 
 
-	void Refrigeration_table::setNumber_of_box(int number) { number_of_box = number; }	// установка количества ящиков холодильного стола
-	int Refrigeration_table::getNumber_of_box() const { return number_of_box; }
+void Refrigeration_table::setNumber_of_box(int number) { number_of_box = number; }	// СѓСЃС‚Р°РЅРѕРІРєР° РєРѕР»РёС‡РµСЃС‚РІР° СЏС‰РёРєРѕРІ С…РѕР»РѕРґРёР»СЊРЅРѕРіРѕ СЃС‚РѕР»Р°
+int Refrigeration_table::getNumber_of_box() const { return number_of_box; }
 
-	void Refrigeration_table::setPower_of_table(float power) { power_of_tabel = power; }	// установка мощности 
-	float Refrigeration_table::getPower_of_table() const { return power_of_tabel; }
+void Refrigeration_table::setPower_of_table(float power) { power_of_tabel = power; }	// СѓСЃС‚Р°РЅРѕРІРєР° РјРѕС‰РЅРѕСЃС‚Рё 
+float Refrigeration_table::getPower_of_table() const { return power_of_tabel; }
